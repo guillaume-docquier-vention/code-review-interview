047fb0d64332bebef122784ea41cf71a73490bbe
guillaume-docquier-vention <guillaume.docquier@vention.cc>
2024-10-08 09:33:32 -0400

    Setup typescript

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,13 @@
 import { CLIENT_URL } from "./constants";
 import { Cluster, PendulumInstance } from "./instances";
 
+process.on("uncaughtException", err => {
+    // eslint-disable-next-line no-console -- This is legit debugging
+    console.log("uncaughtException...");
+    // eslint-disable-next-line no-console -- This is legit debugging
+    console.error(err);
+});
+
 const cluster = new Cluster([
     new PendulumInstance(3001, CLIENT_URL),
     new PendulumInstance(3002, CLIENT_URL),
